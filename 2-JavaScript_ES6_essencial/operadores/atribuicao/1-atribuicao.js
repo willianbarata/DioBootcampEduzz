008f73303d509b5706b47e266a30f0984ae4a89f
Willian Barata <willianbarata@gmail.com>
2021-12-12 11:14:47 -0300

    Operadores de Atribuição

---
+++ 2-JavaScript_ES6_essencial/operadores/atribuicao/1-atribuicao.js
@@ -0,0 +1,22 @@
+//atribuição
+x =y;
+
+//atribuição de adição 
+x = x + y //ou
+x += y
+
+//atribuição de subtração
+x = x - y //ou
+x -= y
+
+//atribuição de multiplicação
+x = x * y
+x *= y
+
+//atribuição de divisão
+x = x / y
+x /= y
+
+//atribuição de resto
+x = x % y
+x %= y
