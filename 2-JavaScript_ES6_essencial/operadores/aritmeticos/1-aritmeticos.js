f2164e5aace3f7fec74e3e882a3f60eed7f2f430
Willian Barata <willianbarata@gmail.com>
2021-12-12 11:03:59 -0300

    Operadores aritmeticos

---
+++ 2-JavaScript_ES6_essencial/operadores/aritmeticos/1-aritmeticos.js
@@ -0,0 +1,29 @@
+//Módulo (%)
+//Operador binário. Retorna o inteiro restante da divisão de dois operandos.
+
+console.log( 12 % 5); // ex. retorna 2 (não é um número par)
+console.log( 12 % 2); // ex. retorna 0 (número par)
+
+//incremento (++) e decremento (--)
+x =0;
+console.log(++x);
+console.log(x++);
+
+console.log(--x);
+console.log(x--);
+
+//Negação (-) e Adição (+)
+console.log(-3);
+console.log(+ "3");
+console.log(+true);
+console.log(+false);
+console.log(-true);
+
+//operador de exponenciação (**)
+
+console.log(2**3);
+console.log(10 ** -1);
+
+//operador de agrupamento ()
+
+console.log(2 * (3 + 2));
