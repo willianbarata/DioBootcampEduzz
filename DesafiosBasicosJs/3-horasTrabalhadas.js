e0b3108048620f781e625174198168d52e3265cf
willianbarata <willianbarata@gmail.com>
2021-12-18 11:43:55 -0200

    valor hora trabalhada

---
+++ DesafiosBasicosJs/3-horasTrabalhadas.js
@@ -0,0 +1,13 @@
+//Precisamos saber quanto uma determinada empresa deve pagar para seus colaboradores, 
+//porém temos apenas a quantidade de horas trabalhadas e o valor hora. Escreva um programa
+// que leia o número de um colaborador, seu número de horas trabalhadas, o valor que recebe 
+//por hora e calcula o salário desse colaborador. Em seguida, apresente o número e o salário 
+//do colaborador, com duas casas decimais.
+
+let valor1 = 25;
+let valor2 = 100;
+let valor3 = 5.50;
+
+let salario = valor2 * valor3;
+
+console.log(salario.toFixed(2));
