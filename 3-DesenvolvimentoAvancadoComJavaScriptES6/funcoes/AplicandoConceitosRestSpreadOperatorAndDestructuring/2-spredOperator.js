f0a56e0a0f578e5f2cf2e1b33b7176aa8543c567
willianbarata <willianbarata@gmail.com>
2021-12-18 10:04:25 -0200

    unindo array com spread operator

--- 3-DesenvolvimentoAvancadoComJavaScriptES6/funcoes/AplicandoConceitosRestSpreadOperatorAndDestructuring/2-spredOperator.js
+++ 3-DesenvolvimentoAvancadoComJavaScriptES6/funcoes/AplicandoConceitosRestSpreadOperatorAndDestructuring/2-spredOperator.js
@@ -16,4 +16,29 @@ function logArgs(...args){
 
 logArgs(...str);
 
+//Utilização em Array
+
+const arr = [1,2,3,4];
+
+function logArgumentos(){
+    console.log(arguments);
+};
+
+logArgumentos(...arr);
+
+
+//unindo array de forma tradicional
+
+const arr2 = [5,6,7].concat(arr);
+
+console.log(arr2);
+
+//unindo array com spread operator
+
+const arr3 = [...arr, 8, 8, 8];
+
+const arr4 = [8, 8, 8, ...arr];
+
+console.log(arr3);
+console.log(arr4);
 
