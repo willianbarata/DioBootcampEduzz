638309fc03d95dcb673b2d62d3f811c4947e8bd1
willianbarata <willianbarata@gmail.com>
2021-12-18 23:16:27 -0200

    interface e extensao

--- 4-ts/index.ts
+++ 4-ts/index.ts
@@ -2,3 +2,22 @@ function soma(a: number, b: number){
     return a + b;
 }
 
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: "terrestre"
+}
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: "terrestre",
+    visaoNoturna: true
+}
