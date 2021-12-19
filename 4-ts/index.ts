658dc54bea254b23053e4cd2becf83d1e503d173
willianbarata <willianbarata@gmail.com>
2021-12-18 23:54:46 -0200

    captando elementos do html com typescript

--- 4-ts/index.ts
+++ 4-ts/index.ts
@@ -34,3 +34,9 @@ const animal: IAnimal = {
    // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis} Db`)
 }
 
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+});
